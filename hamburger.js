75bb2d11066850b2d9e51bc95029a101e8969a70
581257 <54100236+581257@users.noreply.github.com>
2019-09-30 12:09:04 +0300

    Add files via upload

---
+++ hamburger.js
@@ -0,0 +1,13 @@
+
+	jQuery(function($){
+    	$( '.menu-btn' ).click(function(){
+    		$('.responsive-menu').addClass('expand')
+    		$('.menu-btn').addClass('btn-none')
+    	})
+    	
+    	 $( '.close-btn' ).click(function(){
+    		$('.responsive-menu').removeClass('expand')
+    		$('.menu-btn').removeClass('btn-none')
+        })
+        
+  	})
