bca840d5b74055b9a657d7b3cd1a4745a4c4380e
h576069 <54098946+h576069@users.noreply.github.com>
2019-09-30 13:52:44 +0200

    Update hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -1,13 +1,13 @@
 
-	jQuery(function($){
-    	$( '.menu-btn' ).click(function(){
-    		$('.responsive-menu').addClass('expand')
-    		$('.menu-btn').addClass('btn-none')
-    	})
-    	
-    	 $( '.close-btn' ).click(function(){
-    		$('.responsive-menu').removeClass('expand')
-    		$('.menu-btn').removeClass('btn-none')
-        })
-        
-  	})
+  function openNav() {
+    hamburger.classList.add("is-active");
+    document.getElementById("res-men").style.display = "block";
+  }
+
+  function closeNav() {
+    hamburger.classList.remove("is-active");
+    document.getElementById("res-men").style.display = "none";
+  }
+
+var hamburger =  document.querySelector(".menu-btn");
+hamburger.addEventListener('click', () => hamburger.classList.contains('is-active') ? closeNav() : openNav());
